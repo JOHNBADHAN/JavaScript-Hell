289b812807931bce80574bd3c1227a428e33a13a
JOHNBADHAN <johnbadhan517@gmail.com>
2026-01-12 18:17:52 +0530

    Initial commit

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 167494
+let accountEamil = "john@gmail.com"
+var accountPassword = "1234567"
+accountCity = "NewYork"
+let accountState
+
+ 
+accountEamil = "wick@gmail.com"
+accountPassword = "09876543"
+accountCity = "Queens"
+
+console.log(accountEamil);
+
+/*Prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.table([accountId, accountEamil, accountPassword, accountCity, accountState]);
+  
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "John"
+let age= 20
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
