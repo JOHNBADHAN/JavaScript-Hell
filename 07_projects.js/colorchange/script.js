30757eb5d686be46f569332621eac393187d4b39
JOHNBADHAN <johnbadhan517@gmail.com>
2026-01-27 19:51:31 +0530

    project

---
+++ 07_projects.js/colorchange/script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body')
+const button = document.querySelectorAll('.button')
+
+button.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'purple') {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
